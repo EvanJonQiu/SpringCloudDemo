51fd0a12c7d99703d1d6b78e53344af3c5d46c88
EvanJonQiu <EvanJonQiu@users.noreply.github.com>
2020-08-16 00:02:19 +0800

    chapter_3

--- licensing-service/src/main/java/com/example/demo/controller/LicenseServiceController.java
+++ licensing-service/src/main/java/com/example/demo/controller/LicenseServiceController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.config.ServiceConfig;
 import com.example.demo.model.License;
 import com.example.demo.services.LicenseService;
 
@@ -16,30 +20,35 @@ import com.example.demo.services.LicenseService;
 public class LicenseServiceController {
 
 	@Autowired
-	public LicenseService licenseService;
-	
-	@RequestMapping(value="/{licenseId}", method=RequestMethod.GET)
-	public License getLicense(
-			@PathVariable("organizationId") String organizationId,
-			@PathVariable("licenseId") String licenseId) {
-		return new License()
-	            .withId(licenseId)
-	            .withOrganizationId(organizationId)
-	            .withProductName("Teleco")
-	            .withLicenseType("Seat");
-	}
-	
-	@RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
+    private LicenseService licenseService;
+
+    @Autowired
+    private ServiceConfig serviceConfig;
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public List<License> getLicenses( @PathVariable("organizationId") String organizationId) {
+
+        return licenseService.getLicensesByOrg(organizationId);
+    }
+
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public License getLicenses( @PathVariable("organizationId") String organizationId,
+                                @PathVariable("licenseId") String licenseId) {
+
+        return licenseService.getLicense(organizationId,licenseId);
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
     public String updateLicenses( @PathVariable("licenseId") String licenseId) {
         return String.format("This is the put");
     }
-	
-	@RequestMapping(value="{licenseId}",method = RequestMethod.POST)
-    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the post");
+
+    @RequestMapping(value="/",method = RequestMethod.POST)
+    public void saveLicenses(@RequestBody License license) {
+       licenseService.saveLicense(license);
     }
-	
-	@RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
         return String.format("This is the Delete");
--- licensing-service/src/main/java/com/example/demo/services/LicenseService.java
+++ licensing-service/src/main/java/com/example/demo/services/LicenseService.java
@@ -1,30 +1,44 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.config.ServiceConfig;
 import com.example.demo.model.License;
+import com.example.demo.repository.LicenseRepository;
 
 @Service
 public class LicenseService {
-	public License getLicense(String licenseId) {
-		return new License()
-				.withId(licenseId)
-				.withOrganizationId(UUID.randomUUID().toString())
-				.withProductName("Test Product Name")
-				.withLicenseType("PerSeat");
+	
+	@Autowired
+	private LicenseRepository licenseRepository;
+	
+	@Autowired
+	ServiceConfig config;
+	
+	public License getLicense(String organizationId,String licenseId) {
+        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        return license.withComment(config.getExampleProperty());
+    }
+	
+	public List<License> getLicensesByOrg(String organizationId){
+        return licenseRepository.findByOrganizationId( organizationId );
+    }
+	
+	public void saveLicense(License license){
+        license.withId( UUID.randomUUID().toString());
+
+        licenseRepository.save(license);
+    }
+	
+	public void updateLicense(License license){
+	      licenseRepository.save(license);
 	}
 	
-	public void saveLicense(License license) {
-		
-	}
-	
-	public void updateLicense(License license) {
-		
-	}
-	
-	public void deleteLicense(License license) {
-		
-	}
+	public void deleteLicense(License license){
+        licenseRepository.deleteById(license.getLicenseId());
+    }
 }
