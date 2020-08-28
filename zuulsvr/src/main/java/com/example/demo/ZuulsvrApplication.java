e9bb4c48d7cd8f85f97b766c625f2151fe17354e
Evan <EvanJonQiu@users.noreply.github.com>
2020-08-29 00:22:39 +0800

    add zuul server

---
+++ zuulsvr/src/main/java/com/example/demo/ZuulsvrApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.utils.UserContextInterceptor;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulsvrApplication {
+
+	@LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors == null) {
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulsvrApplication.class, args);
+	}
+
+}
